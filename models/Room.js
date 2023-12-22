3a8f388bea8fcc6818b2a96e1727ec4ebef93e36
christian goddu <rgoddu909@gmail.com>
2023-12-21 23:06:37 -0500

    index file to describe model relations

--- models/Room.js
+++ models/Room.js
@@ -1,6 +1,5 @@
 //room table includes id and name
 //room id uses uuid - this will be presented on the page as the key for entering a room
-
 const { Model, DataTypes } = require('sequelize')
 
 const sequelize = require('../config/connection')
@@ -12,8 +11,10 @@ Room.init(
         id: {
             type: DataTypes.UUID,
             defaultValue: DataTypes.UUIDV4,
-            allowNull: false,
-            primaryKey: true
+            primaryKey: true,
+            //when we delete a room, we don't want to delete the associated users.... either the api or the front end will have to handle null values so that rooms with null id's are not displayed
+            allowNull: true,
+            onDelete: 'SET NULL'
         },
         name: {
             type: DataTypes.STRING,
--- models/index.js
+++ models/index.js
@@ -1,1 +1,44 @@
-const Room = 
+//'room => post' relation is 'one => many'
+//'user => post' relation is 'one => many'
+//'user => room' relation is 'many <=> many' through room_user table
+
+const User = require('./User');
+const Room = require('./Room');
+const Post = require('./Post');
+const RoomUser = require('./RoomUser');
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
+User.hasMany(Post,{
+    foreignKey: 'user_id'
+})
+
+Post.belongsTo(Room, {
+    foreignKey: 'room_id',
+    onDelete: 'CASCADE'
+})
+Room.hasMany(Post,{
+    foreignKey: 'room_id'
+})
+
+User.belongsToMany(Room, {
+    through: {
+        model: RoomUser,
+        unique: false
+    }
+})
+Room.belongsToMany(User, {
+    through: {
+        model: RoomUser,
+        unique: false
+    }
+})
+
+module.exports = {
+    User,
+    Room,
+    Post,
+    RoomUser
+}
