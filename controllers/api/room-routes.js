29f5ae973aa3ba62163fa3a0d02121c3893c9c81
christian goddu <rgoddu909@gmail.com>
2023-12-22 15:24:14 -0500

    route to get all users

---
+++ controllers/api/room-routes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router()
+const { Room } = require('../../models')
+
+router.post('/', async (req, res) => {
+
+})
---
+++ controllers/api/user-routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router()
+const { User, Room } = require('../../models')
+
+router.get('/', async(req,res) => {
+    try{
+        const userData = await User.findAll({
+            include: [{model: Room}]
+        });
+        res.status(200).json(userData)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
