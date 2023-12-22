c0c6a15458181b11a45ff2f26a45fab25f95d602
christian goddu <rgoddu909@gmail.com>
2023-12-21 21:08:54 -0500

    added sequilize connection to connection.js

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+process.env.JAWSDB_URL 
+? sequelize = new Sequelize(process.env.JAWSDB_URL)
+:
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+
+
+module.exports = sequelize;
