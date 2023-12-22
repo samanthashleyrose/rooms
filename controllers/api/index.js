99f331c7968d3ef870a45a1367b0bf5805391779
christian goddu <rgoddu909@gmail.com>
2023-12-22 15:07:03 -0500

    created files for api routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
+const postRoutes = require('./post-routes')
+const userRoutes = require('./user-routes')
+const roomRoutes = require('./room-routes')
 
-
+router.use('/posts', postRoutes)
+router.use('/users', userRoutes)
+router.use('/rooms', roomRoutes)
 
 module.exports = router;
