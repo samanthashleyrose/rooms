1b7a29cbea14738002850ec1ba73144b03b81933
Dakota Blanchard <dakotablanchard2699@gmail.com>
2024-01-02 19:15:21 -0500

    set up basic GET routes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,50 @@
+// Import modules
+const router = require('express').Router();
+
+// Setting the default route to /home
+router.get('/', async (req, res) => {
+    res.redirect('/home');
+});
+
+// Route for HOME screen
+router.get('/home', async (req, res) => {
+    try {
+        res.render('homepage')
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Route for JOIN ROOM
+router.get('/join-room', async (req, res) => {
+    try {
+        res.render('join-room')
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Route for LOGIN
+router.get('/login', async (req, res) => {
+    try {
+        res.render('login', {
+            style: 'login.css'
+        })
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Route for ROOM
+router.get('/room/:id', async (req, res) => {
+    try {
+        res.render('room')
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+// Export the router
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
-//routes '/rooms' request to the api folder
-
 const router = require('express').Router();
-const apiRoutes = require('./api');
 
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+
+// Routes '/rooms' request to the api folder, '/' request to the homeRoutes.js file
+router.use('/', homeRoutes);
 router.use('/rooms', apiRoutes);
 
 module.exports = router;
