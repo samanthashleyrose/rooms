dd10239992683c4884c5cc35e371a849fb77d414
christian goddu <rgoddu909@gmail.com>
2024-01-04 18:41:34 -0500

    asoidfj

--- public/js/login.js
+++ public/js/login.js
@@ -1,15 +1,20 @@
-async function login() {
-    try{const response = await fetch('/rooms/users', {
-        method: 'GET'
-    });
-    const users = await response.json()
-    console.log(users)
-}
-    catch(err){
+// var users
+async function getAllUsers() {
+   let users
+    try {
+        const response = await fetch('/rooms/users', {
+            method: 'GET'
+        });
+        users = await response.json()
+        // console.log(users)
+        // return users
+    }
+    catch (err) {
         console.log(err)
     }
     // const lettuce = Promise.resolve(users)
-    // return user
+    return users
 }
-// var user = login()
+// var user = getAllUsers()
 // console.log(user)
+getAllUsers().then(console.log)
